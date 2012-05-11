4e20944bebf0d42c24871ce104c44309002bacb3
Gonçalo Queirós <mail@goncaloqueiros.net>
2012-05-11 19:04:26 +0100

    Added the highcharts, dynamic charts examples

---
+++ demos/highcharts/dynamic/click_to_add_a_point.php
@@ -0,0 +1,58 @@
+<?php
+include_once "../../../Highchart.php";
+
+$chart = new Highchart();
+
+$chart->chart->renderTo = "container";
+$chart->chart->type = "scatter";
+$chart->chart->margin = array(70, 50, 60, 80);
+
+$chart->chart->events->click = new HighchartJsExpr("function(e) {
+    var x = e.xAxis[0].value,
+        y = e.yAxis[0].value,
+        series = this.series[0];
+    series.addPoint([x, y]); }");
+
+$chart->title->text = "User supplied data";
+$chart->subtitle->text = "Click the plot area to add a point. Click a point to remove it.";
+$chart->xAxis->minPadding = 0.2;
+$chart->xAxis->maxPadding = 0.2;
+$chart->xAxis->maxZoom = 60;
+$chart->yAxis->title->text = "Value";
+$chart->yAxis->minPadding = 0.2;
+$chart->yAxis->maxPadding = 0.2;
+$chart->yAxis->maxZoom = 60;
+
+$chart->yAxis->plotLines[] = array('value' => 0,
+                                   'width' => 1,
+                                   'color' => "#808080");
+
+$chart->legend->enabled = false;
+$chart->exporting->enabled = false;
+$chart->plotOptions->series->lineWidth = 1;
+
+$chart->plotOptions->series->point->events->click = new HighchartJsExpr("function() {
+    if (this.series.data.length > 1) this.remove(); }");
+
+$chart->series[]->data = array(array(20,20), array(80,80));
+?>
+
+<html>
+  <head>
+    <title>Click to add a point</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <?php
+      foreach ($chart->getScripts() as $script) {
+         echo '<script type="text/javascript" src="' . $script . '"></script>';
+      }
+    ?>
+  </head>
+  <body>
+    <div id="container"></div>
+    <script type="text/javascript">
+    <?php
+      echo $chart->render("chart1");
+    ?>
+    </script>
+  </body>
+</html>
---
+++ demos/highcharts/dynamic/spline_updating_each_second.php
@@ -0,0 +1,72 @@
+<?php
+include_once "../../../Highchart.php";
+
+$chart = new Highchart();
+
+$chart->chart->renderTo = "container";
+$chart->chart->type = "spline";
+$chart->chart->marginRight = 10;
+
+$chart->chart->events->load = new HighchartJsExpr("function() {
+    var series = this.series[0];
+    setInterval(function() {
+        var x = (new Date()).getTime(),
+            y = Math.random();
+            series.addPoint([x, y], true, true);
+    }, 1000); }");
+
+$chart->title->text = "Live random data";
+$chart->xAxis->type = "datetime";
+$chart->xAxis->tickPixelInterval = 150;
+$chart->yAxis->title->text = "Value";
+
+$chart->yAxis->plotLines[] = array('value' => 0,
+                                   'width' => 1,
+                                   'color' => "#808080");
+
+$chart->tooltip->formatter = new HighchartJsExpr("function() {
+    return '<b>'+ this.series.name +'</b><br/>'+
+    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+    Highcharts.numberFormat(this.y, 2); }");
+
+$chart->legend->enabled = false;
+$chart->exporting->enabled = false;
+$chart->series[0]->name = "Random data";
+
+$chart->series[0]->data = new HighchartJsExpr("(function() {
+    var data = [],
+        time = (new Date()).getTime(),
+        i;
+
+    for (i = -19; i <= 0; i++) {
+        data.push({
+            x: time + i * 1000,
+            y: Math.random()
+        });
+    }
+    return data; })()");
+
+$globalOptions = new HighchartOption();
+$globalOptions->global->useUTC = false;
+?>
+
+<html>
+  <head>
+    <title>Spline updating each second</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <?php
+      foreach ($chart->getScripts() as $script) {
+         echo '<script type="text/javascript" src="' . $script . '"></script>';
+      }
+    ?>
+  </head>
+  <body>
+    <div id="container"></div>
+    <script type="text/javascript">
+    <?php
+      echo Highchart::setOptions($globalOptions);
+      echo $chart->render("chart1");
+    ?>
+    </script>
+  </body>
+</html>
